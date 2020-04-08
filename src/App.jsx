c4f967ba7eb8b776ba19f73e5a0a100cba85f478
Dan Matthews <dan@cryptobuilders.io>
2020-04-08 10:57:41 +0300

    use HashRouter instead of BrowserRouter for better IPFS compat

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { view } from '@risingstack/react-easy-state';
 
